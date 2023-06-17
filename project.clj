387b2ed4d65443fde1a09443fc34f090aa42b7f2
Bruno Ledesma <led.bruno@gmail.com>
2023-06-17 12:46:23 -0300

    adding random initial code

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject tailor-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject tailor "0.1.0-SNAPSHOT"
+  :description "Helps you write code automation prompts"
+  :url "https://github.com/ledbruno/tailor/"
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns tailor-template.core})
+  :repl-options {:init-ns tailor.core})
--- src/tailor_template/core.clj
+++
@@ -1,6 +0,0 @@
-(ns tailor-template.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
