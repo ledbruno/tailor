644d77111d959a52b0f47113faafa92d2c1ca84c
Bruno Ledesma <led.bruno@gmail.com>
2023-06-17 13:00:38 -0300

    docstring

--- src/tailor/core.clj
+++ src/tailor/core.clj
@@ -1,6 +1,6 @@
 (ns tailor.core)
 
-;(defonce default-context
+;(defonce prompt-menu
  ; {:create-unit-test {:briefing "You are a code assistant, that helps to generate code from the instructions below:"
   ;                    :task "Your task is to create one or many unit tests to the target code."
    ;                   :restrictions "Use only the library clojure test. Your output should be just clojure test code. Do not comment your ideas of how you get to the result, just the resultant clojure expression"}
@@ -8,12 +8,19 @@
  ;                 :task "Your task is to change the target code, replacing all variable values to the replacement value provided"
   ;                :restrictions "Your output should be just clojure code. Do not comment your ideas of how you get to the result, just the resultant clojure expression"}})
 
-(defonce default-context {})
+(defonce prompt-menu {})
 
 (defn- resolve-file [snippet]
   (slurp (:path snippet)))
 
-(defn code [snippet]
+(defn code 
+  "Loads source code from the into a snippet
+  Supported snippet types
+  - :file -> loads the whole file
+  - TODO! :file-chunk -> loads a piece of a file
+  - TODO! :clojure-ns -> loads the source of a Clojure namespace from the repl
+  - TODO! :clojure-symbol -> loads the source of a Clojure symbol from the repl"
+  [snippet]
   (cond
     (= :file (:type snippet)) (assoc-in snippet [:code] (resolve-file snippet))))
 
@@ -29,19 +36,14 @@
 (defn- snippets [snippets]
   (str "This are some code snippets, that you can use as context on your task. \n" (clojure.string/join (map snippet-and-desc snippets))))
 
-(with-code {:snippets [{:type :file
-                        :path (str (System/getenv "HOME") "/dev/clojure/code-context/project.clj")}]})
+(defn register 
+  "Adds a new prompt context to the menu"
+  [key template]
+  (assoc-in prompt-menu [key] (with-code template)))
 
-(code {:type :file
-       :path (str (System/getenv "HOME") "/dev/clojure/code-context/project.clj")})
-
-(code-snippets [{:type :file
-                 :path (str (System/getenv "HOME") "/dev/clojure/code-context/project.clj")}])
-
-(defn register [key template]
-  (assoc-in default-context [key] (with-code template)))
-
-(defn prompt [context]
+(defn prompt 
+  "Builds a promt string from the context provided"
+  [context]
   (clojure.string/join "\n" [(:briefing context) (snippets (:snippets context)) (:task context) (:restrictions context)]))
 
 
