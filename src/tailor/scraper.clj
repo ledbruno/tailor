ffe92da95a21c08965d2a02469428578707ae90c
Bruno Ledesma <led.bruno@gmail.com>
2023-06-22 23:39:14 -0300

    adding scrapper

---
+++ src/tailor/scraper.clj
@@ -0,0 +1,23 @@
+(ns tailor.scraper
+  (:require [clj-kondo.core :as clj-kondo]
+            [clojure.java.shell :as shell]))
+
+(defn var-named [var-name {var-defs :var-definitions}]
+  (filter #(= var-name (:name %)) var-defs))
+
+#_(defn parent-def [var-name analysis]
+    (map #(find-var-defs (:from-var %) analysis) (filter #(= var-name (:name %)) (:var-usages analysis))))
+
+(defn find-var-def
+  [symbol-str file-path]
+  (var-named (symbol symbol-str) (:analysis (clj-kondo/run!
+                                             {:lint [file-path]
+                                              :config {:analysis true}}))))
+(defn cut [from to file-path]
+  (when (and from to) (shell/sh "sed" "-n" (str from "," to "p") file-path)))
+
+(defn scrap [def-str file]
+  (let [def-found (first (find-var-def def-str file))]
+    (:out (cut (:row def-found) (:end-row def-found) file))))
+
+(scrap "my-fn" "/tmp/foo.clj")
