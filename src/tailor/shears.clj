8cfe62148fe9a78ede300108235adfc47076b247
Bruno Ledesma <bruno.ledesma@nubank.com.br>
2023-06-23 14:32:53 -0300

    adding tests and evolving

--- src/tailor/shears.clj
+++ src/tailor/shears.clj
@@ -2,22 +2,25 @@
   (:require [clj-kondo.core :as clj-kondo]
             [clojure.java.shell :as shell]))
 
-(defn var-named [var-name {var-defs :var-definitions}]
+(defn- var-named [var-name {var-defs :var-definitions}]
   (filter #(= var-name (:name %)) var-defs))
 
 #_(defn parent-def [var-name analysis]
     (map #(find-var-defs (:from-var %) analysis) (filter #(= var-name (:name %)) (:var-usages analysis))))
 
-(defn find-var-def
+(defn- find-var-defs
   [symbol-str file-path]
+  "Returns a list of var defs with that matches the given symbol-str"
   (var-named (symbol symbol-str) (:analysis (clj-kondo/run!
                                              {:lint [file-path]
                                               :config {:analysis true}}))))
-(defn cut [from to file-path]
+
+(defn- cut [from to file-path]
   (when (and from to) (shell/sh "sed" "-n" (str from "," to "p") file-path)))
 
-(defn def-shear [def-str file]
-  (let [def-found (first (find-var-def def-str file))]
-    (:out (cut (:row def-found) (:end-row def-found) file))))
+(defn- at-root [found-defs]
+  )
 
-(def-shear "my-fn" "/tmp/foo.clj")
+(defn shear-def-at-root [def-str file]
+  (let [def-found (at-root (find-var-defs def-str file))]
+    (:out (cut (:row def-found) (:end-row def-found) file))))
