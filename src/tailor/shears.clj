3cf0a84691616fc674188c4e1b52ddad928ea99c
Bruno Ledesma <led.bruno@gmail.com>
2023-11-14 16:12:37 -0300

    preparing for deep shear

--- src/tailor/shears.clj
+++ src/tailor/shears.clj
@@ -1,6 +1,8 @@
 (ns tailor.shears
-  (:require [clj-kondo.core :as clj-kondo]
-            [clojure.java.shell :as shell]))
+  (:require
+   [clj-kondo.core :as clj-kondo]
+   [clojure.java.shell :as shell]
+   [clojure.set :as set]))
 
 (defn index-by [key-fn coll]
   (into {} (map (juxt key-fn identity) coll)))
@@ -8,7 +10,7 @@
 (defn- var-named [var-name {var-defs :var-definitions}]
   (filter #(= var-name (:name %)) var-defs))
 
-(defn- find-var-defs
+(defn find-var-defs
   "Returns a list of var defs with that matches the given symbol-str"
   [symbol-str file-path]
   (let  [analysis (:analysis (clj-kondo/run!
@@ -23,20 +25,38 @@
 (defn- analyzed-ns [which analysis]
   (:name (which (:namespace-definitions analysis))))
 
-(defn at-root
-  "Returns the first var def on the root level, considering only the first namespace declared"
-  [{analysis :analysis matches :matches}]
+(defn top-level-forms
+  "Returns all top level forms, considering only the first namespace declared"
+  [analysis]
   ;if more than one should throw an error?
-  (let [ns (analyzed-ns first analysis)
-        ns-root-level-forms (filter #(and (not (contains? % :from-var)) (= (:from %) ns)) (:var-usages analysis))
-        root-level-row-matches (clojure.set/intersection (set (map :row ns-root-level-forms)) (set (map :row matches)))]
-    ;maybe map all, not only the first found
-    (get (index-by :row matches) (first root-level-row-matches))))
+  (let [ns (analyzed-ns first analysis)]
+    (filter #(and (not (contains? % :from-var)) (= (:from %) ns)) (:var-usages analysis))))
+
+(defn rows [usages-or-defs]
+  (set (map :row usages-or-defs)))
+
+(defn row-match
+  "Find vars that are declared on same row as top level forms. WARNING: brittle approach"
+  [matches top-level-forms]
+  (set/intersection (rows top-level-forms) (rows matches)))
+
+(defn mark [matches top-level-form-matches]
+  ;maybe map all, not only the first match
+  (get (index-by :row matches) (first top-level-form-matches)))
+
+(defn shear [root-matches file-path]
+  (:out (cut (:row root-matches) (:end-row root-matches) file-path)))
 
 (defn var-def-at-root
   "Returns the source code of the var defined at the root level
    def-str : the name of the target var
    file    : path to the clj source code file"
   [def-str file]
-  (let [def-found (at-root (find-var-defs def-str file))]
-    (:out (cut (:row def-found) (:end-row def-found) file))))
+  (let [result  (find-var-defs def-str file)
+        matches (:matches result)
+        def-found (mark matches (row-match matches (top-level-forms (:analysis result))))]
+    (shear def-found file)))
+
+(comment
+  (defn cleanup [var-usage]
+    (assoc (select-keys var-usage [:ns :row :name :end-row]))))
--- test/tailor/shears_test.clj
+++ test/tailor/shears_test.clj
@@ -3,8 +3,8 @@
             [tailor.shears :refer [var-def-at-root]]))
 
 (deftest test-shear-def-at-root
-  (testing "Cuts a simple def at root"
+  (testing "Shears a simple def top level"
     (is (= "(def x \"banana\")\n" (var-def-at-root "x" "./testResources/sample.clj"))))
 
-  (testing "Cuts a simple defn at root"
+  (testing "Shears a simple defn top level"
     (is (= "(defn my-fn []\n  (def x \"orange\")\n  (print \"bla\"))\n" (var-def-at-root "my-fn" "./testResources/sample.clj")))))
